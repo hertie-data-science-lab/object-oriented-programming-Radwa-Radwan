54ffad63be748b772249727c501ec28e495a2757
Radwa-Radwan <112707526+Radwa-Radwan@users.noreply.github.com>
2023-02-20 02:07:56 +0200

    code_doc

--- River.py
+++ River.py
@@ -15,13 +15,15 @@ class River:
     def __init__(self, n_rooms):
         self.n_rooms = deque(n_rooms)
 
-
+# creates eco system based on creatures count
     def create_eco(self, Creature, count):
         eco = random.sample(range(len(self.n_rooms)), count)
         for position in eco:
             if self.n_rooms[position] is None:
                 self.n_rooms[position] = Creature()
 
+#checks if instance is Bear, populates if another Bear was found in the left of right cell. Movement is then randomly decided left or right.
+    # Makes sure position is within deque range and bear then eats Fish if found and the bear's previous position takes value None.
     def move_bears(self):
         for position, creature in enumerate(self.n_rooms):
             if isinstance(creature, Bear):
@@ -39,6 +41,8 @@ class River:
                         elif isinstance(self.n_rooms[new_position], Fish):
                             self.n_rooms[position] = None
 
+# checks if instance is Fish, populates if another Fish was found in the left of right cell. Movement is then randomly decided left or right.
+# Makes sure position is within deque range. Fish is eaten if a bear was close in the new position and previous position takes the value None.
     def move_fish(self):
         for position, creature in enumerate(self.n_rooms):
             if isinstance(creature, Fish):
--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from Creatures import Fish
 from collections import deque
 import random
 
-
+#eco system created is within the range of rooms and animals simulated.
+# positions the fish based on the number of fish and the bears are allocated based on the positions left which do not include already allocated fish.
+#both creatures' allocation is random
 n_rooms = [None]*15
 n_fish = 5
 n_bears = 5
